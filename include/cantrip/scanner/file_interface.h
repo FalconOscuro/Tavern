0e139c14aa6cf056a7da25247758fb8d194be557
Peter <p.jlgarrod@gmail.com>
2025-04-13 17:45:24 +0100

    file interface implementation

--- include/cantrip/scanner/file_interface.h
+++ include/cantrip/scanner/file_interface.h
@@ -10,10 +10,14 @@ namespace cantrip {
 
 struct file_pos
 {
-    uint32_t col;
-    uint32_t row;
+    uint32_t col = 0;
+    uint32_t row = 0;
 
-    uint32_t indent;
+    uint32_t indent = 0;
+
+    // NOTE: Equality operators do not account for indent, they only compare the absolute file position
+    bool operator==(const file_pos& rhs) const;
+    bool operator!=(const file_pos& rhs) const;
 }; /* end of struct file_pos */
 
 class file_interface
@@ -27,18 +31,30 @@ public:
     file_interface(const file_interface&) = delete;
     void operator=(const file_interface&) = delete;
 
-    bool open();
-    bool is_open() const;
+    inline bool open() {
+        return m_file->open();
+    };
 
-    void close();
+    inline bool is_open() const {
+        return m_file->is_open();
+    };
 
-    char peek_char(long offset = 0) const;
+    inline void close() {
+        m_file->close();
+    }
+
+    inline char peek_char(long offset = 0) const {
+        return m_file->peek_char(offset);
+    }
+
     char get_char();
     void pop(size_t count = 1);
 
-    bool eof() const;
+    inline bool eof() const {
+        return m_file->eof();
+    }
 
-    const file_pos& pos() const {
+    inline const file_pos& pos() const {
         return m_pos;
     }
 
@@ -49,6 +65,8 @@ private:
     std::unique_ptr<tavern::file::ifile> m_file;
 
     file_pos m_pos;
+
+    bool m_new_line = true;
 
 }; /* end of class file_interface */
 
--- src/cantrip/scanner/file_interface.cpp
+++ src/cantrip/scanner/file_interface.cpp
@@ -1,5 +1,68 @@
 #include "cantrip/scanner/file_interface.h"
 
+// using common value of 4 spaces per tab character
+#define SPACES_PER_TAB 4
+
 namespace cantrip {
+
+bool file_pos::operator==(const file_pos& rhs) const {
+    return col == rhs.col && row == rhs.row;
+}
+
+bool file_pos::operator!=(const file_pos& rhs) const {
+    return col != rhs.col || row != rhs.row;
+}
+
+file_interface::file_interface(tavern::file::file_handle& file):
+    m_file(file.release())
+{}
+
+char file_interface::get_char() {
+    char c = m_file->get_char();
+
+    update_file_pos(c);
+
+    return c;
+}
+
+void file_interface::pop(size_t count) {
+    for (size_t i = 0; i < count && !m_file->eof(); ++i)
+        update_file_pos(m_file->get_char());
+}
+
+void file_interface::update_file_pos(char c)
+{
+    // end of file
+    if (c == EOF && m_file->eof())
+        return;
+
+    switch (c)
+    {
+    // enter newline state
+    case '\n':
+        ++m_pos.row;
+        m_pos.col = 0;
+        m_pos.indent = 0;
+        m_new_line = true;
+        return;
+
+    // increment indent if in newline state
+    case '\t':
+        if (m_new_line)
+            m_pos.indent += SPACES_PER_TAB;
+        break;
+
+    case ' ':
+        if (m_new_line)
+            ++m_pos.indent;
+        break;
+
+    // drop out of newline state
+    default:
+        m_new_line = false;
+    }
+
+    ++m_pos.col;
+}
 
 } /* end of namespace cantrip */
