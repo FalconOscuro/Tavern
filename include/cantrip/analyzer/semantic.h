814f46bfb455ecca501d27b1ca6fd0f5b45317ac
Peter Garrod <p.jlgarrod@gmail.com>
2025-10-08 13:44:32 +0100

    Separated out analyzer environment class into separate file

--- include/cantrip/analyzer/semantic.h
+++ include/cantrip/analyzer/semantic.h
@@ -2,49 +2,15 @@
 #define SEMANTIC_ANALYZER_H
 
 #include "cantrip/ast/visitor.h"
-
-#include "cantrip/module/module.h"
 #include "cantrip/ast/statement/var_declare.h"
 
-#include <deque>
+#include "cantrip/module/module.h"
+
 #include <string_view>
-#include <unordered_map>
+
+#include "environment.h"
 
 namespace cantrip::analyzer {
-
-// defines the scope for a set of variables/functions
-struct environment
-{
-    // need better way for storing variable types?
-    std::unordered_map<std::string, const ast::var_declare*> variables;
-
-    bool is_loop = false;
-};
-
-struct environment_stack
-{
-    // check for matching var declares at this level
-    bool push_var(const ast::var_declare* var);
-    const ast::var_declare* check_identifier(const ast::identifier* identifier) const;
-
-    bool in_loop() const;
-
-    void push(bool is_loop = false) {
-        env_stack.emplace_back().is_loop = is_loop;
-    }
-
-    void pop() {
-        env_stack.pop_back();
-    }
-
-    environment& top() {
-        return env_stack.back();
-    };
-
-    std::deque<environment> env_stack;
-
-    // Store current type on top of stack for expression resolution
-};
 
 class semantic final : public ast::visitor
 {
---
+++ src/cantrip/analyzer/environment.cpp
@@ -0,0 +1,49 @@
+#include "cantrip/analyzer/environment.h"
+
+#include "cantrip/ast/expression/identifier.h"
+
+namespace cantrip::analyzer {
+
+bool environment_stack::push_var(const ast::var_declare* var)
+{
+    // always ensure stack is populated
+    if (env_stack.empty())
+        push();
+
+    environment& env = top();
+
+    const std::string_view name = var->name;
+
+    // redefinition
+    if (env.variables.find(name.data()) != env.variables.end())
+        return false;
+
+    else {
+        env.variables.emplace(std::make_pair(name, var));
+        return true;
+    }
+}
+
+const ast::var_declare* environment_stack::check_identifier(const ast::identifier* identifier) const
+{
+    for (size_t i = 0; i < env_stack.size(); ++i) {
+        const environment& env = env_stack[env_stack.size() - (i + 1)];
+        auto found = env.variables.find(identifier->name);
+
+        if (found != env.variables.end())
+            return found->second;
+    }
+
+    return nullptr;
+}
+
+bool environment_stack::in_loop() const
+{
+    for (size_t i = 0; i < env_stack.size(); ++i)
+        if (env_stack[i].is_loop)
+            return true;
+
+    return false;
+}
+
+} /* namespace cantrip::analyzer */
